226a84c71f98a119e58a918ab8728e0d9434b2fa
ConorCurry <conorcurry@gmail.com>
2016-02-10 18:51:31 -0500

    Implemented Token class

---
+++ src/Token.java
@@ -0,0 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Token implements UserToken {
+
+    private String gsName;
+    private String username;
+    private ArrayList<String> accessibleGroups;
+
+    public UserToken(String gsName, String username, ArrayList<String> accessibleGroups) {
+        this.gsName = gsName;
+        this.username = username;
+        this.accessibleGroups = new ArrayList<String>(accessibleGroups);
+    }
+
+    public String getIssuer() {
+        return gsName;        
+    }
+    public String getSubject() {
+        return username;
+    }
+    public List<String> getGroups() {
+        return accessibleGroups;
+    }
+}
