0f5db31fe98e5048680b9f6ef9445afdf6659e8f
ConorCurry <conorcurry@gmail.com>
2016-02-10 18:59:23 -0500

    Fixed token constructor

--- src/Token.java
+++ src/Token.java
@@ -7,7 +7,7 @@ public class Token implements UserToken {
     private String username;
     private ArrayList<String> accessibleGroups;
 
-    public UserToken(String gsName, String username, ArrayList<String> accessibleGroups) {
+    public Token(String gsName, String username, ArrayList<String> accessibleGroups) {
         this.gsName = gsName;
         this.username = username;
         this.accessibleGroups = new ArrayList<String>(accessibleGroups);
