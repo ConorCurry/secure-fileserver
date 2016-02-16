8d14b239d705f2e171354d901eb4a8862f7b57ff
ConorCurry <conorcurry@gmail.com>
2016-02-15 22:19:14 -0500

    Less complicated case checking

--- src/ClientApp.java
+++ src/ClientApp.java
@@ -98,10 +98,6 @@ public class ClientApp
                 System.out.println("Sorry, Your choice is not valid, please enter a valid number.");
                 continue;
             }
-            if(choice >= 0 && choice < 14 && choice > 9 && !fileClient.isConnected()) {
-                System.out.println("Invalid option while not connected to a fileserver");
-                continue;
-            }
             if(choice < 0 || choice > 14)
             {
                 System.out.println("Sorry, Your choice is not valid, please enter a valid number.");
